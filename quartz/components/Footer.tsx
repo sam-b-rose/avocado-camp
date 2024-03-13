6577864d3c9f823e8291aa07cd86debadb25395d
Sam Rose <11774595+samrose3@users.noreply.github.com>
2024-03-13 08:25:56 -0400

    Replace Explorer with recent notes and writing

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          © {year} <a href="https://samrose.me">Sam Rose</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
